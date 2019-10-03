6e7aa9012fdfdfb04f80afffbc255166fad20379
whosAle <abelgrave95@gmail.com>
2019-10-03 17:16:41 -0400

    add BookList component

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import Book from './Book';
+
+const BookList = (props) => {
+
+  const { books } = props;
+
+  return();
+
+}
+
+export default BookList;
