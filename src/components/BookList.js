afd47cc69abbd44e145a0f2fd5223629f7e35beb
whosAle <abelgrave95@gmail.com>
2019-10-15 15:59:34 -0400

    src book data

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 
 import Book from './Book';
 
+
+/*
+  Sample book data is provided thanks to Nano Taboada available here:
+  https://gist.github.com/nanotaboada/6396437
+*/
+import booksData from '../data/books.json';
+
 const BookList = (props) => {
 
   const { books } = props;
